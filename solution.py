22424919e32e54792e212f3171578e878aba7ba0
Elina Akhmanova <ahmanovaelina@gmail.com>
2018-08-29 21:38:33 +0400

    Add blanks to the functions of Senator

--- solution.py
+++ solution.py
@@ -1,3 +1,6 @@
+from sys import stdout
+
+
 class Senator:
     def __init__(self, n, senator_id):
         self.n = n
@@ -5,6 +8,20 @@ class Senator:
         self.senator_id = senator_id
         self.laws_list = []
         self.vote = Vote()
+        self.promise_time = 0
+        self.vote_attempts = 0
+
+    def start_vote(self):
+        pass
+
+    def send_vote(self):
+        pass
+
+    def get_propose(self):
+        pass
+
+    def get_query(self):
+        pass
 
 
 class Vote:
@@ -27,10 +44,23 @@ class Vote:
         return True
 
     def vote(self, senator, law):
-        if self.is_voted and (self.who_voted is not senator or self.voted_law is not law):
+        if self.is_voted and (self.who_voted is not senator or self.voted_law is not law)\
+                or self.is_promise and(self.who_promise is not senator or self.voted_law is not law):
             return False
 
         self.is_voted = True
         self.who_voted = senator
         self.voted_law = law
         return True
+
+
+def main():
+    idx = input().split(' ')
+    senator = Senator(int(id([0])), int(idx[1]) - 1)
+
+    while True:
+        request = input()
+
+
+if __name__ == '__main__':
+    main()
